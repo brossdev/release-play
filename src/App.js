22d110ebf35cbd3a1d88af6c83772c5687c2b9e0
Bobby Ross <bobby.dev@pm.me>
2021-03-31 10:58:03 +0100

    fix: casing on app message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Make me beautiful
+          Make me Beautiful!!!
         </a>
       </header>
     </div>
